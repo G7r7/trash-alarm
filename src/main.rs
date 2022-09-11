8d9a9fec66b07d22c20355651dc05f881c48333f
Germain Rullier <grullier@free.fr>
2022-09-11 14:55:56 +0000

    Cleaned up blinky

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,25 @@
-//! # Pico Blinky Example
-//!
-//! Blinks the LED on a Pico board.
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for
-//! the on-board LED.
-//!
-//! See the `Cargo.toml` file for Copyright and license details.
-
 #![no_std]
 #![no_main]
 
-use cortex_m::delay::Delay;
-use embedded_hal::digital::v2::InputPin;
-// The macro for our start-up function
-use rp_pico::entry;
-
-// GPIO traits
 use embedded_hal::digital::v2::OutputPin;
-
 // Ensure we halt the program on panic (if we don't mention this crate it won't
 // be linked)
 use panic_halt as _;
 
-use rp_pico::hal::gpio::Output;
-use rp_pico::hal::gpio::Pin;
-use rp_pico::hal::gpio::PinId;
-use rp_pico::hal::gpio::PushPull;
-// Pull in any important traits
-use rp_pico::hal::prelude::*;
-
-// A shorter alias for the Peripheral Access Crate, which provides low-level
-// register access
-use rp_pico::hal::pac;
-
-// A shorter alias for the Hardware Abstraction Layer, which provides
-// higher-level drivers.
-use rp_pico::hal;
-use rp_pico::hal::rtc::DateTime;
-use rp_pico::hal::rtc::RealTimeClock;
-
-///&mut &mut  Entry point to our bare-metal application.
-///
 /// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
 /// as soon as all global variables are initialised.
-///
-/// The function configures the RP2040 peripherals, then blinks the LED in an
-
-fn blink_led<T: PinId>(led_pin: &mut Pin<T, Output<PushPull>>, ms: u32, delay: &mut Delay) {
-    led_pin.set_high().unwrap();
-    led_pin.set_high().unwrap();
-    delay.delay_ms(ms);
-    led_pin.set_low().unwrap();
-    delay.delay_ms(ms);
-}
-
-/// infinite loop.
-#[entry]
+#[rp_pico::entry]
 fn main() -> ! {
     // Grab our singleton objects
-    let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
+    let mut pac = rp_pico::pac::Peripherals::take().unwrap();
+    let core = rp_pico::pac::CorePeripherals::take().unwrap();
 
     // Set up the watchdog driver - needed by the clock setup code
-    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    let mut watchdog = rp_pico::hal::Watchdog::new(pac.WATCHDOG);
 
     // Configure the clocks
-    //
     // The default is to generate a 125 MHz system clock
-    let clocks = hal::clocks::init_clocks_and_plls(
+    let clocks = rp_pico::hal::clocks::init_clocks_and_plls(
         rp_pico::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
@@ -79,26 +31,15 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    // Real Time Clock
-    let date_time = DateTime {
-        day: 10,
-        month: 09,
-        year: 2022,
-        day_of_week: hal::rtc::DayOfWeek::Sunday,
-        hour: 23,
-        minute: 55,
-        second: 0,
-    };
-
-    let real_time_clock =
-        RealTimeClock::new(pac.RTC, clocks.rtc_clock, &mut pac.RESETS, date_time).unwrap();
-
     // The delay object lets us wait for specified amounts of time (in
     // milliseconds)
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let mut delay = cortex_m::delay::Delay::new(
+        core.SYST,
+        rp_pico::hal::Clock::freq(&clocks.system_clock).to_Hz(),
+    );
 
     // The single-cycle I/O block controls our GPIO pins
-    let sio = hal::Sio::new(pac.SIO);
+    let sio = rp_pico::hal::Sio::new(pac.SIO);
 
     // Set the pins up according to their function on this particular board
     let pins = rp_pico::Pins::new(
@@ -108,27 +49,13 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Set the LED to be an output
-    let mut led_pin = pins.led.into_push_pull_output();
-    let _red_led_pin = pins.gpio1.into_push_pull_output();
-
-    // Set the red button to be an input
-    let button_pin = pins.gpio28.into_pull_up_input();
-
-    // Flag to store the activation alarm state
-    let mut alarm_state = true;
-
-    // Blink speed
-    let blink_time_ms = 20;
+    let mut led = pins.led.into_push_pull_output();
 
     // Blink the LED at 1 Hz
     loop {
-        if button_pin.is_low().unwrap() {
-            alarm_state = false;
-        }
-
-        if alarm_state {
-            blink_led(&mut led_pin, blink_time_ms, &mut delay);
-        }
+        led.set_high().unwrap();
+        delay.delay_ms(500);
+        led.set_low().unwrap();
+        delay.delay_ms(500);
     }
 }
