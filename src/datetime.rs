f98760f71a0b478376e7c3cc1f66429eb24af6d0
Germain Rullier <grullier@free.fr>
2022-09-24 15:33:37 +0000

    Phases for setting the datetime, with blinking cursor.

--- src/datetime.rs
+++ src/datetime.rs
@@ -1,8 +1,9 @@
 use arrayvec::ArrayString;
 use core::fmt::{Error, Write};
 use cortex_m::delay::Delay;
+// use defmt::write;
 use embedded_hal::{blocking::serial::write, digital::v2::InputPin};
-use lcd_1602_i2c::Lcd;
+use lcd_1602_i2c::{Blink, Lcd};
 use rp_pico::{
     hal::{
         self,
@@ -13,7 +14,6 @@ use rp_pico::{
     pac::I2C0,
 };
 
-const BUTTON_PHASE_DAY_OF_WEEK: u8 = 0;
 
 pub trait FormatToArrayString {
     fn to_date_arraystring(&self) -> ArrayString<10>;
@@ -38,8 +38,47 @@ fn get_time_of_day_string(day_of_week: DayOfWeek) -> ArrayString<8> {
         DayOfWeek::Saturday => "Samedi",
         DayOfWeek::Sunday => "Dimanche",
     };
+
     let mut ret_arrstr = ArrayString::<8>::new();
     write!(ret_arrstr, "{: <8}", ret).unwrap();
+    // write!(ret_arrstr, "{}", ret).unwrap();
+    return ret_arrstr;
+}
+
+#[derive(PartialEq)]
+enum ButtonPhase {
+    ButtonPhaseDayOfWeek = 0,
+    ButtonPhaseTimeHourTens = 1,
+    ButtonPhaseTimeHourUnits = 2,
+    ButtonPhaseTimeMinuteTens = 3,
+    ButtonPhaseTimeMinuteUnits = 4,
+    ButtonPhaseFinished = 5,
+}
+
+fn get_button_phase_from_u8(button_phase:&u8) -> ButtonPhase {
+    return match button_phase {
+        0 => ButtonPhase::ButtonPhaseDayOfWeek,
+        1 => ButtonPhase::ButtonPhaseTimeHourTens,
+        2 => ButtonPhase::ButtonPhaseTimeHourUnits,
+        3 => ButtonPhase::ButtonPhaseTimeMinuteTens,
+        4 => ButtonPhase::ButtonPhaseTimeMinuteUnits,
+        5 => ButtonPhase::ButtonPhaseFinished,
+        _ => ButtonPhase::ButtonPhaseFinished,
+    }
+}
+fn get_button_phase_string(button_phase: &ButtonPhase) -> ArrayString<16>{
+    let ret = match button_phase {
+        ButtonPhase::ButtonPhaseDayOfWeek => "Jour Semaine ?",
+        ButtonPhase:: ButtonPhaseTimeHourTens => "Dizaine Heure ?",
+        ButtonPhase:: ButtonPhaseTimeHourUnits => "Unite Heure ?",
+        ButtonPhase:: ButtonPhaseTimeMinuteTens => "Dizaine Minute ?",
+        ButtonPhase:: ButtonPhaseTimeMinuteUnits => "Unite Minute ?",
+        ButtonPhase:: ButtonPhaseFinished => "Finished ?",
+        _ => "WTF ?",
+    };
+
+    let mut ret_arrstr = ArrayString::<16>::new();
+    write!(ret_arrstr, "{: <16}", ret).unwrap();
     // write!(ret_arrstr, "{}", ret).unwrap();
     return ret_arrstr;
 }
@@ -70,20 +109,33 @@ impl FromScreenAndButtons for DateTime {
         validate_button: &mut Pin<VP, Input<PullUp>>,
     ) -> Self {
         // Année
+        let mut datetime = DateTime {
+            year: 0,
+            day: 1,
+            month: 1,
+            day_of_week: DayOfWeek::Monday,
+            hour: 0,
+            minute: 0,
+            second: 0,
+        };
         let mut day_of_week = DayOfWeek::Monday;
-        let mut year_string = ArrayString::<4>::new();
         lcd.clear(delay).unwrap();
 
-        let mut button_phase = BUTTON_PHASE_DAY_OF_WEEK;
-        let mut str_button_phase = "Jour semaine ?";
+        let mut button_phase = ButtonPhase::ButtonPhaseDayOfWeek;
+
         loop {
             if validate_button.is_low().unwrap() {
-                break;
-            }
-            lcd.set_cursor_position(0, 0).unwrap();
-            lcd.write_str(str_button_phase).unwrap();
+                let mut incr_button_phase= button_phase as u8 + 1;
+                button_phase = get_button_phase_from_u8(&incr_button_phase);
+                lcd.clear(delay).unwrap();
+                while validate_button.is_low().unwrap() {}
 
-            if button_phase == BUTTON_PHASE_DAY_OF_WEEK {
+            }
+
+            lcd.set_cursor_position(0, 0).unwrap();
+            lcd.write_str(get_button_phase_string(&button_phase).as_str()).unwrap();
+
+            if button_phase == ButtonPhase::ButtonPhaseDayOfWeek {
                 // lcd.set_cursor_position(0, 1).unwrap();
                 // lcd.write_str("        ").unwrap();
                 lcd.set_cursor_position(0, 1).unwrap();
@@ -93,18 +145,31 @@ impl FromScreenAndButtons for DateTime {
                     day_of_week = day_of_week_from_u8((day_of_week as u8 + 1) % 7);
                     while increment_button.is_low().unwrap() {}
                 }
+            } else {
+                lcd.set_cursor_position(0, 1).unwrap();
+                lcd.write_str(&datetime.to_time_arraystring()[0..5]).unwrap();
             }
+            match button_phase {
+                ButtonPhase::ButtonPhaseTimeHourUnits => blink_digit(lcd, 1),
+                ButtonPhase::ButtonPhaseTimeHourTens => blink_digit(lcd, 0),
+                ButtonPhase::ButtonPhaseTimeMinuteTens => blink_digit(lcd, 3),
+                ButtonPhase::ButtonPhaseTimeMinuteUnits => blink_digit(lcd, 4),
+                ButtonPhase::ButtonPhaseFinished => continue,
+                ButtonPhase::ButtonPhaseDayOfWeek => continue,
+            }
+            // We wait for the next user input.
+            while !increment_button.is_low().unwrap() && !validate_button.is_low().unwrap() {}
         }
-        return DateTime {
-            year: 2022,
-            day: 10,
-            month: 09,
-            day_of_week: day_of_week,
-            hour: 23,
-            minute: 55,
-            second: 0,
-        };
+        return datetime;
     }
+}
+
+fn blink_digit<DP: PinId + BankPinId, CP: PinId + BankPinId>(
+    lcd: &mut Lcd<I2C<I2C0, (Pin<DP, Function<gpio::I2C>>, Pin<CP, Function<gpio::I2C>>)>>,
+    pos_digit: u8
+) {
+    lcd.set_cursor_position(pos_digit, 1).unwrap();
+    lcd.set_blink(Blink::On).unwrap();
 }
 
 impl FormatToArrayString for DateTime {
